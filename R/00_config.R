92075a67231d203985b11f7ff49a96c3eafc9681
Alexander-Morin <alexm.14@outlook.com>
2024-09-22 15:39:59 -0700

    adding microglia object paths

---
+++ R/00_config.R
@@ -0,0 +1,22 @@
+
+## TODO: These are paths from the TRsc paper, consider dependancy
+
+# TRsc metadata
+sc_meta_path <- "/space/grp/amorin/Metadata/single_cell_dataset_meta.tsv"
+
+# Location of aggregate coexpression matrices
+amat_dir <- "/space/scratch/amorin/TR_singlecell/"
+
+# List of cell types per dataset
+celltype_list_path <- "/space/scratch/amorin/R_objects/celltype_list.RDS"
+
+##
+
+
+
+# Microglia metadata path
+mcg_meta_path <- "/space/scratch/amorin/aggregate_microglia/microglia_metadata.tsv"
+
+
+# Microglia list of mat/meta
+mcg_dat_path <- "/space/scratch/amorin/aggregate_microglia/microglia_dat_list.RDS"
