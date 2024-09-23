8ca64e0086bc9b45640890c6b32443dda42a4dfe
Alexander-Morin <alexm.14@outlook.com>
2024-09-23 15:46:22 -0700

    msr and summary

--- R/0X_avg_counts_per_dat.R
+++ R/0X_avg_counts_per_dat.R
@@ -2,7 +2,7 @@
 ## -----------------------------------------------------------------------------
 
 library(tidyverse)
-# library(aggtools)
+library(aggtools)
 library(pheatmap)
 library(parallel)
 source("R/00_config.R")
@@ -17,16 +17,54 @@ ids_mm <- unique(filter(mcg_meta, Species == "Mouse")$ID)
 dat_l <- readRDS(mcg_dat_path)
 
 
-avg_hg <- do.call(cbind, lapply(dat_l[ids_hg], function(x) rowMeans(x$Mat)))
-avg_mm <- do.call(cbind, lapply(dat_l[ids_mm], function(x) rowMeans(x$Mat)))
+# TODO: summarization into a function
+# TODO: add more rank-based summarization; weigh against scaling counts
+# TODO: is parallel helping or slowing
 
+
+avg_hg <- do.call(cbind, mclapply(dat_l[ids_hg], function(x) rowMeans(x$Mat), mc.cores = ncore))
+avg_mm <- do.call(cbind, mclapply(dat_l[ids_mm], function(x) rowMeans(x$Mat), mc.cores = ncore))
+
+med_hg <- do.call(cbind, mclapply(dat_l[ids_hg], function(x) apply(x$Mat, 1, median), mc.cores = ncore))
+med_mm <- do.call(cbind, mclapply(dat_l[ids_mm], function(x) apply(x$Mat, 1, median), mc.cores = ncore))
 
 sd_hg <- do.call(cbind, mclapply(dat_l[ids_hg], function(x) apply(x$Mat, 1, sd), mc.cores = ncore))
 sd_mm <- do.call(cbind, mclapply(dat_l[ids_mm], function(x) apply(x$Mat, 1, sd), mc.cores = ncore))
 
-
 cv_hg <- sd_hg / avg_hg
 cv_mm <- sd_mm / avg_mm
+
+msr_hg <- do.call(cbind, mclapply(dat_l[ids_hg], function(x) rowSums(zero_sparse_cols(x$Mat)) == 0, mc.cores = ncore))
+msr_mm <- do.call(cbind, mclapply(dat_l[ids_mm], function(x) rowSums(zero_sparse_cols(x$Mat)) == 0, mc.cores = ncore))
+
+
+
+
+summ_hg <- data.frame(
+  Symbol = rownames(avg_hg),
+  Avg = rowMeans(avg_hg),
+  Med = rowMeans(med_hg),
+  SD = rowMeans(sd_hg),
+  CV = rowMeans(cv_hg),
+  N_msr = rowSums(msr_hg)
+)
+
+
+
+summ_mm <- data.frame(
+  Symbol = rownames(avg_mm),
+  Avg = rowMeans(avg_mm),
+  Med = rowMeans(med_mm),
+  SD = rowMeans(sd_mm),
+  CV = rowMeans(cv_mm),
+  N_msr = rowSums(msr_mm)
+)
+
+
+
+
+all0_hg <- filter(Symbol)
+
 
 
 
@@ -34,6 +72,10 @@ cv_mm <- sd_mm / avg_mm
 # TODO: compare cor using SD/CV, and cor after remove low genes
 
 dat_cor_hg <- cor(avg_hg, method = "spearman")
+
+
+
+
 dat_cor_mm <- cor(avg_mm, method = "spearman")
 
 
@@ -56,3 +98,12 @@ cor_heatmap(dat_cor_hg)
 cor_heatmap(dat_cor_mm)
 
 
+
+
+
+# More deviation between avg and median at lower end
+plot(summ_hg$Avg, summ_hg$Med)
+plot(summ_hg$Avg[summ_hg$Avg < 1000 & summ_hg$Med], summ_hg$Med[summ_hg$Avg < 1000 & summ_hg$Med])
+
+plot(summ_hg$Avg, summ_hg$SD)
+plot(summ_hg$Med, summ_hg$CV)
