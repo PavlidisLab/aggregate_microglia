b2e1a3bf6fd4b87887bef66744f5c44f971b012f
Alexander-Morin <alexm.14@outlook.com>
2024-09-24 10:26:50 -0700

    rm mclapply; init sample function for large datasets

--- R/0X_cell_cor_per_dat.R
+++ R/0X_cell_cor_per_dat.R
@@ -10,18 +10,32 @@ mcg_meta <- read.delim(mcg_meta_path)
 dat_l <- readRDS(mcg_dat_path)
 
 
+mcg_meta <- mcg_meta %>% 
+  group_by(ID) %>% 
+  summarise(N_cells = sum(N_cells)) %>% 
+  arrange(N_cells)
+
+
 # Note that I inspected taking cor after filtering away 0 genes, but saw
 # trivial change to summary stats
 
+# TODO: check cell count and subsample at threshold
+
 calc_cell_cor <- function(dat_l) {
   
-  cor_l <- lapply(dat_l, function(x) {
+  cor_l <- lapply(names(dat_l), function(x) {
     
-    message(paste(x, Sys.time()))
+    print(message(paste(x, Sys.time())))
     
-    cell_cor <- aggtools::sparse_pcor(x$Mat)
-    cell_cor <- mat_to_df(cell_cor, value_name = "Cor")
-    summary(cell_cor$Cor)
+    result <- tryCatch({
+      cell_cor <- aggtools::sparse_pcor(dat_l[[x]]$Mat)
+      cell_cor <- mat_to_df(cell_cor, value_name = "Cor")
+      summary(cell_cor$Cor)
+    }, 
+    error = function(e) NULL)
+    
+    return(result)
+    
   })
   names(cor_l) <- names(dat_l)
   
@@ -29,24 +43,70 @@ calc_cell_cor <- function(dat_l) {
 }
 
 
-if (!file.exists(cell_cor_path)) {
-  cell_cor <- calc_cell_cor(dat_l)
-  saveRDS(cell_cor, cell_cor_path)
-} else {
-  cell_cor <- readRDS(cell_cor_path)
+
+calc_sample_cell_cor <- function(dat_l, 
+                                 # mcg_meta, 
+                                 n_cell_threshold = 10e3, 
+                                 n_sample_cells = 1e3,
+                                 n_samples = 10) {
+  
+  cor_l <- lapply(names(dat_l), function(x) {
+    
+    mat <- dat_l[[x]]$Mat
+    
+    sample_l <- lapply(1:n_samples, function(i) {
+      sample_cells <-  sample(colnames(mat), n_sample_cells, replace = FALSE)
+      submat <- mat[, sample_cells]
+      cell_cor <- aggtools::sparse_pcor(submat)
+      cell_cor <- mat_to_df(cell_cor, value_name = "Cor")[["Cor"]]
+    })
+    
+    summary(unlist(sample_l))
+    
+    names(sample_l) <- names(dat_l)
+    
+    return(cor_l)
+  })
 }
 
 
 
-print(length(cell_cor))
+
+
+if (!file.exists(cell_cor_path)) {
+  cor_l <- calc_cell_cor(dat_l)
+  saveRDS(cor_l, cell_cor_path)
+} else {
+  cor_l <- readRDS(cell_cor_path)
+}
 
 
 
-# id <- "GSE121891"
-# cell_cor <- aggtools::sparse_pcor(dat_l[[id]]$Mat)
-# cell_cor <- mat_to_df(cell_cor, value_name = "Cor")
-# summary(cell_cor$Cor)
-# plot(density(cell_cor$Cor))
+# TODO: some elements failed in lapply call, but can be regen isolated...
+# figure out how to do one pass
+
+null_cor <- mcg_meta %>% 
+  filter(ID %in% names(which(sapply(cor_l, function(x) is.null(x))))) %>% 
+  arrange(N_cells)
+
+
+rerun_cor_ids <- filter(null_cor, N_cells < 20e3) %>% pull(ID) %>% unique()
+sample_cor_ids <- setdiff(null_cor$ID, rerun_cor_ids)
+
+
+rerun_cor <- calc_cell_cor(dat_l[rerun_cor_ids])
+
+
+
+subsample_cor <- calc_sample_cell_cor(dat_l[sample_cor_ids])
+
+
+
+test_id <- "GSE160523"
+test_cell_cor <- aggtools::sparse_pcor(dat_l[[id]]$Mat)
+test_cell_cor <- mat_to_df(cell_cor, value_name = "Cor")
+summary(test_cell_cor$Cor)
+plot(density(test_cell_cor$Cor))
 # 
 # keep_genes <- names(which(rowMeans(dat_l[[id]]$Mat) != 0))
 # cell_cor_keep <- aggtools::sparse_pcor(dat_l[[id]]$Mat[keep_genes, ])
