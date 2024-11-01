fb9037020e39286c45e5a5dd5837b9ff39cb1fd2
Alexander-Morin <alexm.14@outlook.com>
2024-11-01 14:46:18 -0700

    fix QN names arg; add rowmeans of QN avg; using min count in one third data heuristic; porting out cluster code

--- R/0X_avg_counts_per_dat.R
+++ R/0X_avg_counts_per_dat.R
@@ -16,6 +16,7 @@ mcg_meta <- read.delim(mcg_meta_path)
 ids_hg <- unique(filter(mcg_meta, Species == "Human")$ID)
 ids_mm <- unique(filter(mcg_meta, Species == "Mouse")$ID)
 
+
 dat_l <- readRDS(mcg_dat_path)
 
 
@@ -43,7 +44,7 @@ summarize_count_list <- function(dat_l) {
   cv <- sd / avg
   
   # Quantile normalize the averaged profiles
-  qn_avg <- preprocessCore::normalize.quantiles(avg, keep.names = FALSE)
+  qn_avg <- preprocessCore::normalize.quantiles(avg, keep.names = TRUE)
   
   # Rank and rank product of the averaged profiles
   rank_avg <- aggtools::colrank_mat(avg)
@@ -58,6 +59,7 @@ summarize_count_list <- function(dat_l) {
   summ_df <- data.frame(
     Symbol = genes,
     Avg = rowMeans(avg, na.rm = TRUE),
+    QN_avg = rowMeans(qn_avg, na.rm = TRUE),
     Med = rowMedians(med, na.rm = TRUE),
     SD = rowMeans(sd, na.rm = TRUE),
     CV = rowMeans(cv, na.rm = TRUE),
@@ -95,250 +97,31 @@ if (!file.exists(count_summ_path)) {
 
 
 
-# Trying to figure out genes with acceptable enough expression to keep
+# Identifying genes to filter
+# NOTE: Median is wildly stringent (median of medians too harsh?)
+# Heuristic: gene must be measured (min 20 cells) in at least a third of datasets
 # ------------------------------------------------------------------------------
 
 summ_df_hg <- count_summ$Human$Summ_df
-cutoff_hg <- 0.3
-hist(summ_df_hg$Avg, breaks = 1000)
-abline(v = cutoff_hg, col = "red")
-
-# keep_hg <- filter(summ_df_hg, N_msr > 0 & Avg > 0 & Med > 0) %>% arrange(desc(Avg))
-# keep_hg <- filter(summ_df_hg, Avg > cutoff_hg) %>% arrange(desc(Avg))
-keep_hg <- filter(summ_df_hg, N_msr > 5) %>% arrange(desc(N_msr))
-# keep_hg <- filter(summ_df_hg, N_msr > 0 & Avg > 0) %>% arrange(desc(Avg))
-
-hist(keep_hg$Avg, breaks = 1000)
-
+cutoff_hg <- floor(length(ids_hg) * (1/3))
+keep_hg <- filter(summ_df_hg, N_msr >= cutoff_hg) %>% arrange(desc(N_msr))
 
 
 summ_df_mm <- count_summ$Mouse$Summ_df
-cutoff_mm <- 0.5
+cutoff_mm <- floor(length(ids_mm) * (1/3))
+keep_mm <- filter(summ_df_mm, N_msr >= cutoff_mm) %>% arrange(desc(Avg))
+
+
+hist(summ_df_hg$Avg, breaks = 1000)
+hist(keep_hg$Avg, breaks = 1000)
+
 hist(summ_df_mm$Avg, breaks = 1000)
-abline(v = cutoff_mm, col = "red")
-
-# keep_mm <- filter(summ_df_mm, N_msr > 0 & Avg > 0 & Med > 0) %>% arrange(desc(Avg))
-# keep_mm <- filter(summ_df_mm, Avg > cutoff_mm) %>% arrange(desc(Avg))
-keep_mm <- filter(summ_df_mm, N_msr > 5) %>% arrange(desc(N_msr))
-# keep_mm <- filter(summ_df_mm, N_msr > 0 & Avg > 0) %>% arrange(desc(Avg))
-
 hist(keep_mm$Avg, breaks = 1000)
 
 
 
 
 
-# Experiment similarity/clustering
-# ------------------------------------------------------------------------------
-
-
-cor_avg_hg <- cor(count_summ$Human$QN_Avg[keep_hg$Symbol, ], method = "spearman")
-cor_avg_mm <- cor(count_summ$Mouse$QN_Avg[keep_mm$Symbol, ], method = "spearman")
-
-cor_cv_hg <- cor(count_summ$CV_hg[keep_hg$Symbol, ], method = "spearman", use = "pairwise.complete.obs")
-cor_cv_mm <- cor(count_summ$CV_mm[keep_mm$Symbol, ], method = "spearman", use = "pairwise.complete.obs")
-
-
-
-
-cor_heatmap <- function(mat) {
-  
-  min_break <-  min(mat)
-  
-  pheatmap(mat,
-           cluster_rows = TRUE,
-           cluster_cols = TRUE,
-           color = viridis::inferno(10),
-           breaks = seq(min(min_break), 1, length.out = 10),
-           border_color = "black",
-           fontsize = 20)
-  
-}
-
-
-cor_heatmap(cor_avg_hg)
-cor_heatmap(cor_avg_mm)
-
-cor_heatmap(cor_cv_hg)
-cor_heatmap(cor_cv_mm)
-
-
-
-hclust_avg_hg <- hclust(d = as.dist(1 - cor_avg_hg))
-hclust_avg_mm <- hclust(d = as.dist(1 - cor_avg_mm))
-
-
-hclust_cv_hg <- hclust(d = as.dist(1 - cor_cv_hg))
-hclust_cv_mm <- hclust(d = as.dist(1 - cor_cv_mm))
-
-
-sil_avg_hg <- cluster::silhouette(cutree(hclust_avg_hg, k = 2), as.dist(1 - cor_avg_hg))
-mean(sil_avg_hg[, "sil_width"])
-plot(sil_avg_hg)
-
-
-sil_avg_mm <- cluster::silhouette(cutree(hclust_avg_mm, k = 2), as.dist(1 - cor_avg_mm))
-mean(sil_avg_mm[, "sil_width"])
-plot(sil_avg_mm)
-
-
-
-
-# TODO: you have to be more explicit about expected shape in since it calls t()
-pca_and_var <- function(mat) {
-  
-  # Performs PCA with prcomp and returns list of the resulting 
-  # object as well as the variance explained
-  
-  # prcomp expects samples as rows, features (genes) as columns so transpose
-  pcmat <- prcomp(t(mat), scale = TRUE)
-  
-  # variance explained by the PCs
-  prc_var <- pcmat$sdev ^ 2
-  var_explained <- round(prc_var / sum(prc_var) * 100, 2)
-  cumvar_explained <- cumsum(var_explained)/sum(var_explained)
-  return(list(PC = pcmat, 
-              Var_explained = var_explained, 
-              Cumvar_explained = cumvar_explained))
-}
-
-
-
-pc_hg <- pca_and_var(count_summ$Avg_hg[keep_hg$Symbol, ])
-pc_mm <- pca_and_var(count_summ$Avg_mm[keep_mm$Symbol, ])
-# pc_hg <- pca_and_var(avg_log_hg[keep_hg$Symbol, ])
-# pc_mm <- pca_and_var(avg_log_mm[keep_mm$Symbol, ])
-
-
-pc_df_hg <- data.frame(pc_hg$PC$x[, 1:5]) %>% rownames_to_column(var = "ID")
-pc_df_mm <- data.frame(pc_mm$PC$x[, 1:5]) %>% rownames_to_column(var = "ID")
-
-
-clus_df_hg <- data.frame(
-  Avg_cluster = cutree(hclust_avg_hg, k = 2),
-  CV_cluster = cutree(hclust_cv_hg, k = 2)) %>% 
-  rownames_to_column(var = "ID") %>% 
-  left_join(filter(mcg_meta, Species == "Human"), by = "ID") %>% 
-  left_join(pc_df_hg, by = "ID") %>% 
-  mutate(Platform = str_replace_all(Platform, " ", ""),
-         Platform = ifelse(is.na(Platform), "Mixed", Platform),
-         Platform = ifelse(sapply(str_split(Platform, ","), length) != 1, "Mixed", Platform),
-         Is_10X = str_detect(Platform, "^10x.*"))
-
-
-
-clus_df_mm <- data.frame(
-  Avg_cluster = cutree(hclust_avg_mm, k = 2),
-  CV_cluster = cutree(hclust_cv_mm, k = 2)) %>% 
-  rownames_to_column(var = "ID") %>% 
-  left_join(filter(mcg_meta, Species == "Mouse"), by = "ID") %>% 
-  left_join(pc_df_mm, by = "ID") %>% 
-  mutate(Platform = str_replace_all(Platform, " ", ""),
-         Platform = ifelse(is.na(Platform), "Mixed", Platform),
-         Platform = ifelse(sapply(str_split(Platform, ","), length) != 1, "Mixed", Platform),
-         Is_10X = str_detect(Platform, "^10x.*"))
-
-
-plot(hclust_avg_hg)
-plot(hclust_avg_mm)
-
-plot(hclust_cv_hg)
-plot(hclust_cv_mm)
-
-
-table(clus_df_hg$Avg_cluster, clus_df_hg$CV_cluster)
-table(clus_df_mm$Avg_cluster, clus_df_mm$CV_cluster)
-
-
-
-# Count of cells by cluster
-boxplot(log10(clus_df_hg$N_cells+1) ~ clus_df_hg$Avg_cluster)
-wilcox.test(clus_df_hg$N_cells ~ clus_df_hg$Avg_cluster)
-# kruskal.test(clus_df_hg$N_cells ~ clus_df_hg$Avg_cluster)
-
-boxplot(log10(clus_df_mm$N_cells+1) ~ clus_df_mm$Avg_cluster)
-wilcox.test(clus_df_mm$N_cells ~ clus_df_mm$Avg_cluster)
-# kruskal.test(clus_df_mm$N_cells ~ clus_df_mm$Avg_cluster)
-
-
-# Median UMI by cluster
-boxplot(log10(clus_df_hg$Median_UMI+1) ~ clus_df_hg$Avg_cluster)
-wilcox.test(clus_df_hg$Median_UMI+1 ~ clus_df_hg$Avg_cluster)
-# kruskal.test(clus_df_hg$Median_UMI ~ clus_df_hg$Avg_cluster)
-
-boxplot(log10(clus_df_mm$Median_UMI+1) ~ clus_df_mm$Avg_cluster)
-wilcox.test(clus_df_mm$Median_UMI+1 ~ clus_df_mm$Avg_cluster)
-# kruskal.test(clus_df_mm$Median_UMI ~ clus_df_mm$Avg_cluster)
-
-
-# Gene msr by cluster
-boxplot(log10(clus_df_hg$N_msr_postfilt+1) ~ clus_df_hg$Avg_cluster)
-wilcox.test(clus_df_hg$N_msr_postfilt+1 ~ clus_df_hg$Avg_cluster)
-# kruskal.test(clus_df_hg$N_msr_postfilt ~ clus_df_hg$Avg_cluster)
-
-boxplot(log10(clus_df_mm$N_msr_postfilt+1) ~ clus_df_mm$Avg_cluster)
-wilcox.test(clus_df_mm$N_msr_postfilt+1 ~ clus_df_mm$Avg_cluster)
-# kruskal.test(clus_df_mm$N_msr_postfilt ~ clus_df_mm$Avg_cluster)
-
-
-fisher.test(table(clus_df_hg$Platform, clus_df_hg$Avg_cluster))
-fisher.test(table(clus_df_mm$Platform, clus_df_mm$Avg_cluster))
-
-fisher.test(table(clus_df_hg$Is_10X, clus_df_hg$Avg_cluster))
-fisher.test(table(clus_df_mm$Is_10X, clus_df_mm$Avg_cluster))
-
-
-round(cor(select_if(clus_df_hg, is.numeric)), 4)
-round(cor(select_if(clus_df_mm, is.numeric)), 4)
-
-
-boxplot(clus_df_hg$PC1 ~ clus_df_hg$Platform)
-boxplot(clus_df_hg$PC2 ~ clus_df_hg$Platform)
-kruskal.test(clus_df_hg$PC1 ~ clus_df_hg$Platform)
-kruskal.test(clus_df_hg$PC2 ~ clus_df_hg$Platform)
-
-boxplot(clus_df_mm$PC1 ~ clus_df_mm$Platform)
-boxplot(clus_df_mm$PC2 ~ clus_df_mm$Platform)
-kruskal.test(clus_df_mm$PC1 ~ clus_df_mm$Platform)
-kruskal.test(clus_df_mm$PC2 ~ clus_df_mm$Platform)
-
-
-boxplot(clus_df_hg$PC1 ~ clus_df_hg$Is_10X)
-boxplot(clus_df_hg$PC2 ~ clus_df_hg$Is_10X)
-wilcox.test(clus_df_hg$PC1 ~ clus_df_hg$Is_10X)
-wilcox.test(clus_df_hg$PC2 ~ clus_df_hg$Is_10X)
-
-boxplot(clus_df_mm$PC1 ~ clus_df_mm$Is_10X)
-boxplot(clus_df_mm$PC2 ~ clus_df_mm$Is_10X)
-kruskal.test(clus_df_mm$PC2 ~ clus_df_mm$Is_10X)
-kruskal.test(clus_df_mm$PC1 ~ clus_df_mm$Is_10X)
-
-
-
-ggplot(clus_df_hg, aes(x = PC1, y = PC2, fill = log10(N_cells))) +
-  geom_point(size = 4, shape = 21) +
-  scale_fill_gradient(low = "white", high = "firebrick") +
-  theme_classic()
-
-
-ggplot(clus_df_mm, aes(x = PC1, y = PC2, fill = log10(N_cells))) +
-  geom_point(size = 4, shape = 21) +
-  scale_fill_gradient(low = "white", high = "firebrick") +
-  theme_classic()
-
-
-
-ggplot(clus_df_hg, aes(x = PC1, y = PC2, fill = log10(Median_UMI))) +
-  geom_point(size = 4, shape = 21) +
-  scale_fill_gradient(low = "white", high = "firebrick") +
-  theme_classic()
-plot(clus_df_hg$PC1, log10(clus_df_hg$Median_UMI+1))
-
-ggplot(clus_df_mm, aes(x = PC1, y = PC2, fill = log10(Median_UMI))) +
-  geom_point(size = 4, shape = 21) +
-  scale_fill_gradient(low = "white", high = "firebrick") +
-  theme_classic()
-plot(clus_df_mm$PC1, log10(clus_df_mm$Median_UMI+1))
 
 
 
