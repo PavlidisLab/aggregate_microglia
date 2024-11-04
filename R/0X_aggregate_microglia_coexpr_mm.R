e50bd62d58b1942238b23706ec98be90cfd643a7
Alexander-Morin <alexm.14@outlook.com>
2024-11-04 13:37:12 -0800

    update with aggtools workflow

--- R/0X_aggregate_microglia_coexpr_mm.R
+++ R/0X_aggregate_microglia_coexpr_mm.R
@@ -1,64 +1,24 @@
+## TODO: save .RDS or just .tsv?
+## -----------------------------------------------------------------------------
+
 library(tidyverse)
 library(data.table)
 library(aggtools)
 source("R/00_config.R")
-# source("R/utils/dev_functions.R")
+source("R/utils/functions.R")
 
-# Table of assembled scRNA-seq datasets
-sc_meta <- read.delim(sc_meta_path, stringsAsFactors = FALSE)
-
-# List of cell types
-ct_l <- readRDS(celltype_list_path)
-
+# Gene table and dataset meta
 pc_df <- read.delim(ref_mm_path, stringsAsFactors = FALSE)
+meta <- read.delim(mcg_meta_path) %>% filter(Species == "Mouse")
 
-# Save out
-out_dir <- "/space/scratch/amorin/TR_singlecell/Microglia/Mm_pcor"
-dir.create(out_dir, showWarnings = FALSE)
-aggr_path <- file.path(out_dir, "aggtools_aggregate_matrix_fishersz.tsv")
-na_path <- file.path(out_dir, "aggtools_NA_matrix_fishersz.tsv")
+# Paths for the Fisher's Z, Rank Sum Rank, and NA tracking matrices
+fz_path <- file.path(cmat_dir_mm, "aggregate_cormat_FZ_mm.RDS")
+rsr_path <- file.path(cmat_dir_mm, "aggregate_cormat_RSR_mm.RDS")
 
-
-
-# Extract IDs of specific cell type of interest
-# ------------------------------------------------------------------------------
-
-
-ct <- "microglia|mcg|mgl|microg"  
-
-
-ct_df <- lapply(ct_l, function(x) {
-  ct_vec <- str_to_lower(x$Ct_count$Cell_type)
-  ct_which <- str_detect(ct_vec, ct)
-  
-  if (sum(ct_which) == 0) {
-    return(NA)
-  }
-  
-  x$Ct_count[ct_which, ]
-  
-})
-
-
-ct_df <- ct_df[!is.na(ct_df)]
-
-
-
-ct_df <- data.frame(
-  do.call(rbind, ct_df)
-) %>% 
-  rownames_to_column(var = "ID") %>% 
-  mutate(ID = str_replace(ID, "\\.[:digit:]+$", "")) %>% 
-  left_join(., sc_meta[, c("ID", "Species")], by = "ID")
-
-
-ct_df <- filter(ct_df, Species == "Mouse")
-
-# Add # GSE118020: Micro (micro grep grabs non-microglia) 
-ct_df <- rbind(ct_df, c("GSE118020", "Micro", 3230, "Mouse"))
-
-
-ct_df$Path <- paste0(amat_dir, ct_df$ID, "/",  ct_df$ID, "_clean_mat_and_meta_CPM.RDS")
+# TODO: keep?
+fz_tsv_path <- file.path(cmat_dir_mm, "aggregate_cormat_FZ_mm.tsv")
+rsr_tsv_path <- file.path(cmat_dir_mm, "aggregate_cormat_RSR_mm.tsv")
+na_tsv_path <- file.path(cmat_dir_mm, "aggregate_cormat_NA_mm.tsv")
 
 
 
@@ -68,33 +28,54 @@ ct_df$Path <- paste0(amat_dir, ct_df$ID, "/",  ct_df$ID, "_clean_mat_and_meta_CP
 
 
 
-aggr_l <- aggr_coexpr_multi_dataset(
-  input_df = ct_df,
-  pc_df = pc_df,
-  cor_method = "pearson",
-  agg_method = "FZ"
+save_function_results(
+  path = fz_path,
+  fun = aggr_coexpr_multi_dataset,
+  args = list(
+    input_df = meta,
+    pc_df = pc_df,
+    cor_method = "pearson",
+    agg_method = "FZ"
+  )
 )
 
 
 
-fwrite(
-  data.frame(aggr_l$Agg_mat, check.names = FALSE),
-  sep = "\t",
-  row.names = TRUE,
-  quote = FALSE,
-  verbose = FALSE,
-  showProgress = FALSE,
-  file = aggr_path
+save_function_results(
+  path = rsr_path,
+  fun = aggr_coexpr_multi_dataset,
+  args = list(
+    input_df = meta,
+    pc_df = pc_df,
+    cor_method = "pearson",
+    agg_method = "RSR"
+  )
 )
 
 
 
-fwrite(
-  data.frame(aggr_l$NA_mat, check.names = FALSE),
-  sep = "\t",
-  row.names = TRUE,
-  quote = FALSE,
-  verbose = FALSE,
-  showProgress = FALSE,
-  file = na_path
-)
+# FZ matrix as .tsv files
+if (!file.exists(fz_tsv_path)) {
+  
+  agg_l <- readRDS(fz_path)
+  fwrite_mat(aggr_l$Agg_mat, fz_tsv_path)
+  
+  if (!file.exists(na_tsv_path)) {
+    fwrite_mat(aggr_l$NA_mat, na_tsv_path)
+  }
+
+}
+
+
+
+# RSR matrix as .tsv files
+if (!file.exists(fz_tsv_path)) {
+  
+  agg_l <- readRDS(fz_path)
+  fwrite_mat(aggr_l$Agg_mat, fz_tsv_path)
+  
+  if (!file.exists(na_tsv_path)) {
+    fwrite_mat(aggr_l$NA_mat, na_tsv_path)
+  }
+  
+}
