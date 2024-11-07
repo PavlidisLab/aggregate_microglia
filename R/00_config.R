d826ecb9476fbbbf4bcbab4c49f4fd5b9fe1fef8
Alexander-Morin <alexm.14@outlook.com>
2024-11-07 14:26:31 -0800

    clean up pathing

--- R/00_config.R
+++ R/00_config.R
@@ -1,8 +1,29 @@
-# TODO: most harmonious dir create 
-# dir.create(out_hg, showWarnings = FALSE)
-# dir.create(out_mm, showWarnings = FALSE)
+## Specify pathing and global vars
+## -----------------------------------------------------------------------------
 
+
+# cores for parallel::
 ncore <- 8
+
+
+# Main data output
+data_out_dir <- "/space/scratch/amorin/aggregate_microglia"
+
+
+# Main plot output
+plot_dir <- "/home/amorin/Plots/aggregate_microglia"
+
+
+# Coexpression and aggregates matrices to these directories
+cmat_dir_hg <- file.path(data_out_dir, "Cormats", "Hg_pcor")
+cmat_dir_mm <- file.path(data_out_dir, "Cormats", "Mm_pcor")
+
+
+if (!dir.exists(data_out_dir)) dir.create(data_out_dir)
+if (!dir.exists(plot_dir)) dir.create(plot_dir)
+if (!dir.exists(cmat_dir_hg)) dir.create(cmat_dir_hg)
+if (!dir.exists(cmat_dir_mm)) dir.create(cmat_dir_mm)
+
 
 
 ## TODO: These are paths from the TRsc paper, FIX dependency
@@ -27,33 +48,20 @@ ens_mm_path <- "/space/grp/amorin/Metadata/ensembl_mouse_protein_coding_105.tsv"
 
 
 
-
-# Save matrices to these directories
-cmat_dir_hg <- "/space/scratch/amorin/aggregate_microglia/Cormats/Hg_pcor/"
-cmat_dir_mm <- "/space/scratch/amorin/aggregate_microglia/Cormats/Mm_pcor/"
-
+# Cell type metadata paths
+mcg_meta_path <-  file.path(data_out_dir, "microglia_metadata.tsv")
+macro_meta_path <-  file.path(data_out_dir, "macrophage_metadata.tsv")
 
 
+# Microglia list of count matrices and meta
+mcg_dat_path <-  file.path(data_out_dir, "microglia_dat_list.RDS")
 
-# Microglia metadata path
-mcg_meta_path <- "/space/scratch/amorin/aggregate_microglia/microglia_metadata.tsv"
-
-# Macrophage metadata path
-macro_meta_path <- "/space/scratch/amorin/aggregate_microglia/macrophage_metadata.tsv"
-
-
-
-
-# Microglia list of mat/meta
-mcg_dat_path <- "/space/scratch/amorin/aggregate_microglia/microglia_dat_list.RDS"
 
 # Cell correlations per dataset
-cell_cor_path <- "/space/scratch/amorin/aggregate_microglia/microglia_cell_cors.RDS"
+cell_cor_path <-  file.path(data_out_dir, "microglia_cell_cors.RDS")
 
+                            
 # Summary of gene counts across experiments
-count_summ_path <- "/space/scratch/amorin/aggregate_microglia/microglia_gene_count_summaries.RDS"
-  
-
-# Summary tables of count
-count_summ_table_hg <- "/space/scratch/amorin/aggregate_microglia/microglia_gene_count_summary_hg.tsv"
-count_summ_table_mm <- "/space/scratch/amorin/aggregate_microglia/microglia_gene_count_summary_mm.tsv"
+count_summ_path <-  file.path(data_out_dir, "microglia_gene_count_summaries.RDS")
+count_summ_table_hg <-  file.path(data_out_dir, "microglia_gene_count_summary_hg.tsv")
+count_summ_table_mm <-  file.path(data_out_dir, "microglia_gene_count_summary_mm.tsv")
