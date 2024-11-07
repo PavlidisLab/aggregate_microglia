42f8673a8f834ac14f67b8470a2e5baf34a1b709
Alexander-Morin <alexm.14@outlook.com>
2024-11-07 13:59:28 -0800

    add from TRsc

--- R/01_install_packages.R
+++ R/01_install_packages.R
@@ -1,1 +1,51 @@
+## Collecting packages used. Note that most would have been installed individually.
+## -----------------------------------------------------------------------------
+
+
+options(repos = "http://cran.rstudio.com/")
+
+
+packages <- c(
+  # "testthat",
+  "parallel",
+  "tidyverse",
+  "BiocManager",
+  # "remotes",
+  "Seurat",
+  # "umap",
+  "pheatmap",
+  # "RcppTOML",
+  # "igraph",
+  # "Hmisc",
+  "Matrix",
+  # "patchwork",
+  "plyr",
+  "DescTools",
+  # "googlesheets4",
+  "ROCR",
+  "data.table",
+  "ggrepel",
+  "qlcMatrix",
+  "microbenchmark",
+  "credentials",
+  "rJava"
+)
+
+
+installed_packages <- packages %in% rownames(installed.packages())
+
+
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+
+
+
+# BiocManager::install("WGCNA")
+BiocManager::install("preprocessCore")
+# BiocManager::install("limma")
+# BiocManager::install("edgeR")
+# BiocManager::install("ComplexHeatmap")
+
+devtools::install_github('PavlidisLab/ermineR')
 devtools::install_github("PavlidisLab/aggtools")
