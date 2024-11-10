3aa484fcb1f1874d1c8b2928036bb12d0022ec6a
Alexander-Morin <alexm.14@outlook.com>
2024-11-10 13:52:18 -0800

    pairwise topk and density plots

--- R/misc/check_minfilter.R
+++ R/misc/check_minfilter.R
@@ -5,6 +5,9 @@ source("R/utils/functions.R")
 source("R/utils/vector_comparison_functions.R")
 
 
+pc_df <- read.delim(ref_mm_path, stringsAsFactors = FALSE)
+
+
 # Paths for the aggregate matrix lists
 fz_path <- file.path(cmat_dir_mm, "aggregate_cormat_FZ_mm.RDS")
 allrank_path <- file.path(cmat_dir_mm, "aggregate_cormat_allrank_mm.RDS")
@@ -32,6 +35,31 @@ genes_len <- rownames(allrank_len)
 genes_str <- rownames(allrank_str)
 
 
+
+
+
+fz_vs_allrank <- pair_colwise_topk(fz, allrank, ncores = ncore)
+fz_vs_colrank <- pair_colwise_topk(fz, colrank, ncores = ncore)
+
+fz_vs_allrank_len <- pair_colwise_topk(fz[genes_len, genes_len], allrank_len[genes_len, genes_len], ncores = ncore)
+fz_vs_colrank_len <- pair_colwise_topk(fz[genes_len, genes_len], colrank_len[genes_len, genes_len], ncores = ncore)
+
+fz_vs_allrank_str <- pair_colwise_topk(fz[genes_str, genes_str], allrank_str[genes_str, genes_str], ncores = ncore)
+fz_vs_colrank_str <- pair_colwise_topk(fz[genes_str, genes_str], colrank_str[genes_str, genes_str], ncores = ncore)
+
+
+plot(density(fz_vs_allrank), col = "red", main = "All genes")
+lines(density(fz_vs_colrank), col = "blue")
+
+plot(density(fz_vs_allrank_len), col = "red", "Lenient cut-off")
+lines(density(fz_vs_colrank_len), col = "blue")
+
+plot(density(fz_vs_allrank_str), col = "red", "Stringent cut-off")
+lines(density(fz_vs_colrank_str), col = "blue")
+view(data.frame(fz_vs_allrank_str))
+
+
+
 check <- "C1qc"
 
 
@@ -53,8 +81,8 @@ df_len <- data.frame(
   FZ = fz[genes_len, check],
   Allrank = allrank[genes_len, check],
   Allrank_len = allrank_len[genes_len, check],
-  Colrank = colrank[genes_len, check]
-  # Colrank_len = colrank_len[genes, check],
+  Colrank = colrank[genes_len, check],
+  Colrank_len = colrank_len[genes_len, check]
 )
 df_len <- filter(df_len, Symbol != check)
 
@@ -67,8 +95,8 @@ df_str <- data.frame(
   FZ = fz[genes_str, check],
   Allrank = allrank[genes_str, check],
   Allrank_str = allrank_str[genes_str, check],
-  Colrank = colrank[genes_str, check]
-  # Colrank_str = colrank_str[genes_str, check]
+  Colrank = colrank[genes_str, check],
+  Colrank_str = colrank_str[genes_str, check]
 )
 df_str <- filter(df_str, Symbol != check)
 
@@ -87,7 +115,6 @@ plot(df_len$FZ, df_len$Colrank)
 plot(df_len$FZ, df_len$Colrank_len)
 
 
-
 cor(select_if(df_str, is.numeric), method = "spearman", use = "pairwise.complete.obs")
 plot(df_str$FZ, df_str$Allrank)
 plot(df_str$FZ, df_str$Allrank_str)
@@ -96,22 +123,32 @@ plot(df_str$FZ, df_str$Colrank_str)
 
 
 
+# Fbxl6 had the lowest Top K in the stringent gene set. Golgb1 had the highest
+# FZ but weak/middling allrank. Ifngr1 had the highest allrank
 
-fz_vs_allrank <- pair_colwise_topk(fz, allrank, ncores = ncore)
-fz_vs_colrank <- pair_colwise_topk(fz, colrank, ncores = ncore)
+cor_paths_mm <- list.files(cmat_dir_mm, full.names = TRUE, pattern = "cormat.tsv")
 
-fz_vs_allrank_len <- pair_colwise_topk(fz[genes_len, genes_len], allrank_len[genes_len, genes_len], ncores = ncore)
-fz_vs_colrank_len <- pair_colwise_topk(fz[genes_len, genes_len], colrank_len[genes_len, genes_len], ncores = ncore)
+check <- "Fbxl6"
+gene1 <- "Golgb1"
+gene2 <- "Ifngr1"
 
-fz_vs_allrank_str <- pair_colwise_topk(fz[genes_str, genes_str], allrank_len[genes_str, genes_str], ncores = ncore)
-fz_vs_colrank_str <- pair_colwise_topk(fz[genes_str, genes_str], colrank_len[genes_str, genes_str], ncores = ncore)
+cor_l <- lapply(cor_paths_mm, function(x) {
+  fread_to_mat(x, genes = pc_df$Symbol, sub_genes = c(gene1, gene2))
+})
 
 
-plot(density(fz_vs_allrank), col = "red")
-lines(density(fz_vs_colrank), col = "blue")
+vec1 <- sapply(cor_l, function(x) x[check, gene1])
+vec1 <- vec1[!is.na(vec1)]
 
-plot(density(fz_vs_allrank_len), col = "red")
-lines(density(fz_vs_colrank_len), col = "blue")
+vec2 <- sapply(cor_l, function(x) x[check, gene2])
+vec2 <- vec2[!is.na(vec2)]
 
-plot(density(fz_vs_allrank_str), col = "red")
-lines(density(fz_vs_colrank_str), col = "blue")
+summary(vec1)
+summary(vec2)
+
+sum(vec1 < 0)
+sum(vec2 < 0)
+
+
+hist(vec1, breaks = 100, main = paste(check, gene1, sep = "-"), xlab = "Pcor")
+hist(vec2, breaks = 100, main = paste(check, gene2, sep = "-"), xlab = "Pcor")
