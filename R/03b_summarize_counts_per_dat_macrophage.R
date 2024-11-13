1fb5496f5b4f1a0518355f113eed8492980aa9b5
Alexander-Morin <alexm.14@outlook.com>
2024-11-12 18:43:55 -0800

    init macro count summary

---
+++ R/03b_summarize_counts_per_dat_macrophage.R
@@ -0,0 +1,29 @@
+## TODO: 
+## -----------------------------------------------------------------------------
+
+library(tidyverse)
+library(aggtools)
+library(pheatmap)
+library(preprocessCore)
+library(matrixStats)
+source("R/00_config.R")
+source("R/utils/functions.R")
+
+# Dataset meta and human/mouse data IDs
+macro_meta <- read.delim(macro_meta_dedup_path)
+ids_hg <- unique(filter(macro_meta, Species == "Human")$ID)
+ids_mm <- unique(filter(macro_meta, Species == "Mouse")$ID)
+
+# List of count matrices and their metadata
+dat_l <- readRDS(macro_dat_path)
+
+
+
+if (!file.exists(macro_count_summ_list_path)) {
+  count_summ_hg <- summarize_gene_counts(dat_l[ids_hg])
+  count_summ_mm <- summarize_gene_counts(dat_l[ids_mm])
+  saveRDS(list(Human = count_summ_hg, Mouse = count_summ_mm), macro_count_summ_list_path)
+} else {
+  count_summ <- readRDS(macro_count_summ_list_path)
+}
+
