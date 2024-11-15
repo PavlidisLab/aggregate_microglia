bbc7974c5fd1d8f855d2c4cc59d3bd7d67e6770e
Alexander-Morin <alexm.14@outlook.com>
2024-11-14 16:55:40 -0800

    cleaning up plotting

--- R/03a_summarize_counts_per_dat_microglia.R
+++ R/03a_summarize_counts_per_dat_microglia.R
@@ -18,52 +18,42 @@ ids_mm <- unique(filter(mcg_meta, Species == "Mouse")$ID)
 dat_l <- readRDS(mcg_dat_path)
 
 
-
 if (!file.exists(mcg_count_summ_list_path)) {
+  
+  # Generate summaries for human and mouse separately
   count_summ_hg <- summarize_gene_counts(dat_l[ids_hg])
   count_summ_mm <- summarize_gene_counts(dat_l[ids_mm])
+  
+  # Save out
   saveRDS(list(Human = count_summ_hg, Mouse = count_summ_mm), mcg_count_summ_list_path)
   write.table(count_summ_hg$Summ_df, sep = "\t", quote = FALSE, row.names = FALSE, file = mcg_count_summ_table_hg)
   write.table(count_summ_mm$Summ_df, sep = "\t", quote = FALSE, row.names = FALSE, file = mcg_count_summ_table_mm)
+
 } else {
+  
   count_summ <- readRDS(mcg_count_summ_list_path)
+
 }
-
-
-stop()
-
-
-
-
-# Identifying genes to filter
-# NOTE: Median is wildly stringent (median of medians too harsh?)
-# Heuristic: gene must be measured (min 20 cells) in at least a third of datasets
-# ------------------------------------------------------------------------------
-
-summ_df_hg <- count_summ$Human$Summ_df
-cutoff_hg <- floor(length(ids_hg) * (1/3))
-keep_hg <- filter(summ_df_hg, N_msr >= cutoff_hg) %>% arrange(desc(N_msr))
-
-
-summ_df_mm <- count_summ$Mouse$Summ_df
-cutoff_mm <- floor(length(ids_mm) * (1/3))
-keep_mm <- filter(summ_df_mm, N_msr >= cutoff_mm) %>% arrange(desc(Avg))
-
-
-
-
 
 
 # Plots
 # ------------------------------------------------------------------------------
 
 
-# Hists before/after filter 
-hist(summ_df_hg$Avg, breaks = 1000)
-hist(keep_hg$Avg, breaks = 1000)
+# Hists of average expression and msr
 
-hist(summ_df_mm$Avg, breaks = 1000)
-hist(keep_mm$Avg, breaks = 1000)
+hist(count_summ$Human$Summ_df$QN_avg, breaks = 1000)
+hist(count_summ$Human$Summ_df[count_summ$Human$Filter_genes, "QN_avg"], breaks = 1000)
+
+hist(count_summ$Human$Summ_df$N_msr, breaks = 1000)
+abline(v = (floor(length(ids_hg) * 1/3) - 0.5), col = "red")
+
+
+hist(count_summ$Mouse$Summ_df$QN_avg, breaks = 1000)
+hist(count_summ$Mouse$Summ_df[count_summ$Mouse$Filter_genes, "QN_avg"], breaks = 1000)
+
+hist(count_summ$Mouse$Summ_df$N_msr, breaks = 1000)
+abline(v = (floor(length(ids_mm) * 1/3) - 0.5), col = "red")
 
 
 
@@ -83,7 +73,7 @@ vbplot <- function(dat_l, gene) {
   ggplot(gene_df, aes(x = ID, y = Log2_count)) +
     geom_violin(fill = "slategrey") +
     geom_boxplot(width = 0.1, fill = "white", outlier.shape = NA) +
-    ylab("Log[[2]] CPM") +
+    ylab(bquote(~Log[2]~ "CPM")) +
     ggtitle(gene) +
     theme_classic() +
     theme(axis.text = element_text(size = 20),
@@ -97,12 +87,16 @@ vbplot <- function(dat_l, gene) {
 
 
 # The max average gene
-vbplot(dat_l[ids_hg], slice_max(keep_hg, QN_avg, n = 1)$Symbol)
-vbplot(dat_l[ids_mm], slice_max(keep_mm, QN_avg, n = 1)$Symbol)
+max_hg <- slice_max(count_summ$Human$Summ_df, QN_avg, n = 1) %>% pull(Symbol)
+max_mm <- slice_max(count_summ$Mouse$Summ_df, QN_avg, n = 1) %>% pull(Symbol)
+vbplot(dat_l[ids_hg], max_hg)
+vbplot(dat_l[ids_mm], max_mm)
 
 # The lowest average gene that was kept
-vbplot(dat_l[ids_hg], slice_min(keep_hg, QN_avg, n = 1)$Symbol)
-vbplot(dat_l[ids_mm], slice_min(keep_mm, QN_avg, n = 1)$Symbol)
+low_hg <- count_summ$Human$Summ_df %>% filter(Symbol %in% count_summ$Human$Filter_genes) %>% slice_min(QN_avg) %>% pull(Symbol)
+low_mm <- count_summ$Mouse$Summ_df %>% filter(Symbol %in% count_summ$Mouse$Filter_genes) %>% slice_min(QN_avg) %>% pull(Symbol)
+vbplot(dat_l[ids_hg], low_hg)
+vbplot(dat_l[ids_mm], low_mm)
 
 # The highest average gene that was removed
 gene_hg <- filter(summ_df_hg, Symbol %!in% keep_hg$Symbol) %>% slice_max(QN_avg, n = 1) %>% pull(Symbol)
@@ -118,7 +112,7 @@ plot_avg_heatmap <- function(summ_l, keep_df) {
   
   # gene_order <- arrange(keep_df, desc(QN_avg))$Symbol
   # gene_order <- filter(summ_l$Summ_df, N_msr == max(N_msr)) %>% arrange(desc(QN_avg)) %>% pull(Symbol)
-  gene_order <- slice_min(summ_l$Summ_df, RP, n = 500) %>% arrange(desc(QN_avg)) %>% pull(Symbol)
+  gene_order <- slice_min(summ_l$Summ_df, RP, n = 30) %>% arrange(desc(QN_avg)) %>% pull(Symbol)
   
   plot_mat <- summ_l$QN_Avg
   plot_mat <- plot_mat[gene_order, ]
@@ -147,10 +141,9 @@ plot_avg_heatmap(count_summ$Mouse, keep_mm)
 
 
 # Plotting binary measurement for coexpression
-# ------------------------------------------------------------------------------
+# TODO: function
 
-
-plot_mat <- count_summ$Msr_hg[keep_hg$Symbol, ] * 1
+plot_mat <- count_summ$Human$Msr[keep_hg$Symbol, ] * 1
 
 
 
@@ -165,7 +158,8 @@ pheatmap(plot_mat,
 
 
 
-plot_mat <- count_summ$Msr_mm[keep_mm$Symbol, ] * 1
+plot_mat <- count_summ$Mouse$Msr[keep_mm$Symbol, ] * 1
+
 
 
 pheatmap(plot_mat,
@@ -176,25 +170,3 @@ pheatmap(plot_mat,
          # breaks = seq(0, 1, length.out = 10),
          border_color = NA,
          fontsize = 20)
-
-
-
-# Plotting counts of a single experiments
-# ------------------------------------------------------------------------------
-
-
-plot_mat <- dat_l$GSE118020$Mat
-plot_mat <- cbind(plot_mat, Average = count_summ$Mouse$QN_Avg[, "GSE118020"])
-
-
-pheatmap(plot_mat,
-         cluster_rows = FALSE,
-         cluster_cols = FALSE,
-         color = RColorBrewer::brewer.pal(9, "Reds"),
-         show_rownames = FALSE,
-         show_colnames = FALSE,
-         # breaks = seq(0, 1, length.out = 10),
-         border_color = NA,
-         fontsize = 20,
-         gaps_col = ncol(plot_mat) - 1)
-
