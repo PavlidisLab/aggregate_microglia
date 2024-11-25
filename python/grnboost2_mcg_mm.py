220178f8fcbfaa01e78561e0a0e4c3a5cf9d5ab3
Alexander-Morin <alexm.14@outlook.com>
2024-11-24 17:02:14 -0800

    move loading into guard and mv modules into utils

--- python/grnboost2_mcg_mm.py
+++ python/grnboost2_mcg_mm.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
 from pathlib import Path
-from arboreto.algo import grnboost2
 from dask.distributed import Client, LocalCluster
 import utils
 import rds2py
-import datetime
+
 
 n_workers=20  # How many workers for dask client
 n_iter=100  # How many times to run GRNBoost2
@@ -18,20 +17,21 @@ out_dir = "/space/scratch/amorin/aggregate_microglia/GRNBoost2"
 
 Path(out_dir).mkdir(exist_ok=True)
 
-mcg_dat = rds2py.read_rds(mcg_dat_path)
-mcg_meta = pd.read_table(mcg_meta_path)
-pc_mm = pd.read_table(pc_mm_path)
-tfs_mm = pd.read_table(tfs_mm_path)
-
-# Isolate human IDs to iterate over dat_list
-ids_mm = mcg_meta[mcg_meta.Species == "Mouse"]["ID"].tolist()
-
-# Init dask local client
-local_cluster = LocalCluster(n_workers=n_workers, threads_per_worker=1)
-custom_client = Client(local_cluster)
-
 
 if __name__ == '__main__':
+
+    # Load input data
+    mcg_dat = rds2py.read_rds(mcg_dat_path)
+    mcg_meta = pd.read_table(mcg_meta_path)
+    pc_mm = pd.read_table(pc_mm_path)
+    tfs_mm = pd.read_table(tfs_mm_path)
+
+    # Isolate human IDs to iterate over dat_list
+    ids_mm = mcg_meta[mcg_meta.Species == "Mouse"]["ID"].tolist()
+
+    # Init dask local client
+    local_cluster = LocalCluster(n_workers=n_workers, threads_per_worker=1)
+    custom_client = Client(local_cluster)
 
     utils.iter_grnboost2_over_list(dat_list=mcg_dat, 
                                    ids = ids_mm, 
