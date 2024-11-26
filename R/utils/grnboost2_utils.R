b1910836448a05a3220424fc81703163335b4571
Alexander-Morin <alexm.14@outlook.com>
2024-11-25 17:28:05 -0800

    ready_networks() now orders genes in list; add fread_mat(); add average_and_save_each_network()

--- R/utils/grnboost2_utils.R
+++ R/utils/grnboost2_utils.R
@@ -2,6 +2,7 @@
 
 library(parallel)
 library(data.table)
+source("R/utils/functions.R")
 
 
 # GRNBoost2 output saved as table of [TF, Gene, Importance_score]
@@ -36,7 +37,7 @@ network_list_to_mat <- function(network_list) {
 
 
 
-# Generate a network/importance score matrix for each output network list
+# Load each network into a list of matrices and consistently order names
 
 ready_networks <- function(paths, ncore) {
   
@@ -44,6 +45,10 @@ ready_networks <- function(paths, ncore) {
     network_list <- read_network_list(x)
     network_mat <- network_list_to_mat(network_list)
   }, mc.cores = ncore)
+  
+  tfs <- Reduce(union, lapply(mat_l, colnames))
+  genes <- Reduce(union, lapply(mat_l, rownames))
+  mat_l <- lapply(mat_l, function(mat) mat[genes, tfs])
   
   return(mat_l)
 }
@@ -53,4 +58,56 @@ ready_networks <- function(paths, ncore) {
 # Element-wise average of all matrices in mat_l. Assumes identical dimensions.
 
 average_mat_list <- function(mat_l)  Reduce("+", mat_l) / length(mat_l)
+
+
+
+# TODO:
+# TODO: consider if necessary or can be made general with functions.R fread
+
+fread_mat <- function(path) {
+  
+  mat <- fread(path, sep = "\t")
+  rownames <- mat[["V1"]]
+  mat <- as.matrix(mat[, -1, drop = FALSE])
+  rownames(mat) <- rownames
+  
+  return(mat)
+}
+
+
+
+
+# TODO
+# TODO: consider if load logic can be used with fread_to_mat()
+# TODO: n_iter is pretty hacky, consider replacing when all is ran
+
+average_and_save_each_network <- function(ids, 
+                                          grn_dir, 
+                                          ncore, 
+                                          # force_resave = FALSE) {
+                                          n_iter = 100) {
+  
+  avg_grn_l <- lapply(ids, function(id) {
+    
+    id_dir <- file.path(grn_dir, id)
+    iter_paths <- list.files(id_dir, full.names = TRUE, pattern = ".*_iter.*")
+    avg_grn_path <- file.path(id_dir, paste0(id, "_average_GRN.tsv"))
+    
+    if (!file.exists(avg_grn_path) || length(iter_paths) < n_iter) {
+
+      mat_l <- ready_networks(iter_paths, ncore)
+      avg_grn <- average_mat_list(mat_l)
+      fwrite_mat(avg_grn, avg_grn_path)
+      avg_grn
+      
+    } else {
+      
+      avg_grn <- fread_mat(avg_grn_path)
+      
+    }
+  })
+  names(avg_grn_l) <- ids
+  
+  return(avg_grn_l)
+}
 
