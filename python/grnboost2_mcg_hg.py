bc0b1597ab8181fee3083f9a407eef2375f596d6
Alexander-Morin <alexm.14@outlook.com>
2024-11-26 10:45:01 -0800

    wrap try block

--- python/grnboost2_mcg_hg.py
+++ python/grnboost2_mcg_hg.py
@@ -19,27 +19,31 @@ Path(out_dir).mkdir(exist_ok=True)
 
 if __name__ == '__main__':
 
-    # Load input data
-    mcg_dat = rds2py.read_rds(mcg_dat_path)
-    mcg_meta = pd.read_table(mcg_meta_path)
-    pc_hg = pd.read_table(pc_hg_path)
-    tfs_hg = pd.read_table(tfs_hg_path)
+    try:
+        # Load input data
+        mcg_dat = rds2py.read_rds(mcg_dat_path)
+        mcg_meta = pd.read_table(mcg_meta_path)
+        pc_hg = pd.read_table(pc_hg_path)
+        tfs_hg = pd.read_table(tfs_hg_path)
 
-    # Isolate human IDs to iterate over dat_list
-    ids_hg = mcg_meta[mcg_meta.Species == "Human"]["ID"].tolist()
+        # Isolate human IDs to iterate over dat_list
+        ids_hg = mcg_meta[mcg_meta.Species == "Human"]["ID"].tolist()
 
-    # Init dask local client
-    local_cluster = LocalCluster(n_workers=n_workers, threads_per_worker=1)
-    custom_client = Client(local_cluster)
+        # Init dask local client
+        local_cluster = LocalCluster(n_workers=n_workers, threads_per_worker=1)
+        custom_client = Client(local_cluster)
 
-    utils.iter_grnboost2_over_list(dat_list=mcg_dat, 
-                                   ids = ids_hg, 
-                                   gene_df=pc_hg, 
-                                   tf_df=tfs_hg, 
-                                   n_iter=n_iter,
-                                   out_root=out_dir,
-                                   client=custom_client)
+        utils.iter_grnboost2_over_list(dat_list=mcg_dat, 
+                                       ids = ids_hg, 
+                                       gene_df=pc_hg, 
+                                       tf_df=tfs_hg, 
+                                       n_iter=n_iter,
+                                       out_root=out_dir,
+                                       client=custom_client)
 
-    # Close connections
-    custom_client.close()
-    local_cluster.close()
+    except Exception as e:
+        print(f"Unexpected error in the main script: {e}")
+        
+    finally: # Close connections
+        custom_client.close()
+        local_cluster.close()
--- python/grnboost2_mcg_mm.py
+++ python/grnboost2_mcg_mm.py
@@ -20,27 +20,31 @@ Path(out_dir).mkdir(exist_ok=True)
 
 if __name__ == '__main__':
 
-    # Load input data
-    mcg_dat = rds2py.read_rds(mcg_dat_path)
-    mcg_meta = pd.read_table(mcg_meta_path)
-    pc_mm = pd.read_table(pc_mm_path)
-    tfs_mm = pd.read_table(tfs_mm_path)
+    try:
+        # Load input data
+        mcg_dat = rds2py.read_rds(mcg_dat_path)
+        mcg_meta = pd.read_table(mcg_meta_path)
+        pc_mm = pd.read_table(pc_mm_path)
+        tfs_mm = pd.read_table(tfs_mm_path)
 
-    # Isolate human IDs to iterate over dat_list
-    ids_mm = mcg_meta[mcg_meta.Species == "Mouse"]["ID"].tolist()
+        # Isolate human IDs to iterate over dat_list
+        ids_mm = mcg_meta[mcg_meta.Species == "Mouse"]["ID"].tolist()
 
-    # Init dask local client
-    local_cluster = LocalCluster(n_workers=n_workers, threads_per_worker=1)
-    custom_client = Client(local_cluster)
+        # Init dask local client
+        local_cluster = LocalCluster(n_workers=n_workers, threads_per_worker=1)
+        custom_client = Client(local_cluster)
 
-    utils.iter_grnboost2_over_list(dat_list=mcg_dat, 
-                                   ids = ids_mm, 
-                                   gene_df=pc_mm, 
-                                   tf_df=tfs_mm, 
-                                   n_iter=n_iter,
-                                   out_root=out_dir,
-                                   client=custom_client)
+        utils.iter_grnboost2_over_list(dat_list=mcg_dat, 
+                                       ids = ids_mm, 
+                                       gene_df=pc_mm, 
+                                       tf_df=tfs_mm, 
+                                       n_iter=n_iter,
+                                       out_root=out_dir,
+                                       client=custom_client)
 
-    # Close connections
-    custom_client.close()
-    local_cluster.close()
+    except Exception as e:
+        print(f"Unexpected error in the main script: {e}")
+        
+    finally: # Close connections
+        custom_client.close()
+        local_cluster.close()
