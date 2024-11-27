f4f481a3aa5473346f32a9c60a9c276d2c72715f
Alexander-Morin <alexm.14@outlook.com>
2024-11-27 15:48:47 -0800

    add try block; check motif hits

--- R/misc/demo_Rcistarget.R
+++ R/misc/demo_Rcistarget.R
@@ -9,6 +9,7 @@ source("R/00_config.R")
 source("R/utils/functions.R")
 
 motif_rank_hg <- importRankings("/space/scratch/amorin/hg38_10kbp_up_10kbp_down_full_tx_v10_clust.genes_vs_motifs.rankings.feather")
+motif_score_hg <- importRankings("/space/scratch/amorin/hg38_10kbp_up_10kbp_down_full_tx_v10_clust.genes_vs_motifs.scores.feather")
 motif_rank_mm <- importRankings("/space/scratch/amorin/mm10_10kbp_up_10kbp_down_full_tx_v10_clust.genes_vs_motifs.rankings.feather")
 
 data(motifAnnotations_hgnc)
@@ -39,18 +40,24 @@ rcistarget_over_tf_list <- function(rank_list,
   
   res_l <- lapply(tfs, function(tf) {
     
-    coexpr_genes <- slice_min(rank_list[[tf]], Rank_aggr_coexpr, n = n)$Symbol
-    bind_genes <- slice_min(rank_list[[tf]], Rank_bind, n = n)$Symbol
     
-    res_coexpr <- cisTarget(coexpr_genes, 
+    tryCatch({
+      
+      coexpr_genes <- slice_min(rank_list[[tf]], Rank_aggr_coexpr, n = n)$Symbol
+      bind_genes <- slice_min(rank_list[[tf]], Rank_bind, n = n)$Symbol
+    
+      res_coexpr <- cisTarget(coexpr_genes, 
+                              motifRankings = motif_rank, 
+                              motifAnnot = motif_anno)
+      
+      res_bind <- cisTarget(bind_genes, 
                             motifRankings = motif_rank, 
                             motifAnnot = motif_anno)
-    
-    res_bind <- cisTarget(bind_genes, 
-                          motifRankings = motif_rank, 
-                          motifAnnot = motif_anno)
-    
-    list(Coexpr = res_coexpr, Bind = res_bind)
+      
+      list(Coexpr = res_coexpr, Bind = res_bind)
+      
+    }, 
+    error = function(e) NULL)
     
   })
   names(res_l) <- tfs
@@ -91,6 +98,7 @@ rcistarget_over_mat <- function(mat,
     
   })
   names(res_l) <- tfs
+  res_l <- res_l[!is.na(res_l)]
   
   return(res_l)
 }
@@ -161,37 +169,103 @@ rcistarget_mcgtf_hg <- readRDS(rcistarget_mcgtf_hg_path)
 rcistarget_mcgtf_mm <- readRDS(rcistarget_mcgtf_mm_path)
 
 
+all_motifs_hg <- unique(motif_anno_hg$TF)
+filter_motifs_hg <- unique(filter(motif_anno_hg, keptInRanking)$TF)
 
 
-rcistarget_tfrank_hg <- rcistarget_over_tf_list(
-  rank_list = tf_rank_hg[1:5],
-  motif_rank = motif_rank_hg, 
-  motif_anno = motif_anno_hg,
-  n = 200)
+rcistarget_mcgtf_hg <- rcistarget_mcgtf_hg[!is.na(rcistarget_mcgtf_hg)]
 
 
 
-tt1 <- lapply(names(rcistarget_tfrank_hg), function(tf) {
+tfrank_check_if_motif_present <- function(list) {
   
-  c(High = any(str_detect(rcistarget_tfrank_hg[[tf]]$TF_highConf, tf)),
-    Low = any(str_detect(rcistarget_tfrank_hg[[tf]]$TF_lowConf, tf)))
+  tfs <- names(list)
   
-})
+  df_tfrank <- bind_rows(lapply(tfs, function(tf) {
+    
+    c(Pos_coexpr_high = any(str_detect(list[[tf]]$Coexpr$TF_highConf, tf)),
+      Pos_coexpr_low = any(str_detect(list[[tf]]$Coexpr$TF_lowConf, tf)),
+      Bind_high = any(str_detect(list[[tf]]$Bind$TF_highConf, tf)),
+      Bind_low = any(str_detect(list[[tf]]$Bind$TF_lowConf, tf)))
+  }))
+  
+  
+  df_tfrank <- data.frame(Symbol = tfs, df_tfrank)
+  
+}
+
+
+mcgrank_check_if_motif_present <- function(list) {
+  
+  tfs <- names(list)
+  
+  df_mcgtf <- bind_rows(lapply(tfs, function(tf) {
+    
+    c(Pos_high = any(str_detect(list[[tf]]$Pos_coexpr$TF_highConf, tf)),
+      Pos_low = any(str_detect(list[[tf]]$Pos_coexpr$TF_lowConf, tf)),
+      Neg_high = any(str_detect(list[[tf]]$Neg_coexpr$TF_highConf, tf)),
+      Neg_low = any(str_detect(list[[tf]]$Neg_coexpr$TF_lowConf, tf)))
+  }))
+  
+  
+  df_mcgtf <- data.frame(Symbol = tfs, df_mcgtf)
+  
+}
 
 
 
-sum(sapply(rcistarget_mcgtf_mm, class) == "try-error")
+df_tfrank_hg <- tfrank_check_if_motif_present(rcistarget_tfrank_hg)
+df_tf_rank_hg <- filter(df_tf_rank_hg, Symbol %in% filter_motifs_hg)
+
+df_tfrank_mm <- tfrank_check_if_motif_present(rcistarget_tfrank_mm)
+
+
+sum(df_tfrank_hg$Pos_coexpr_high) / nrow(df_tfrank_hg)
+sum(df_tfrank_hg$Bind_high) / nrow(df_tfrank_hg)
+
+# sum(df_tfrank_hg$Pos_coexpr_high | df_tfrank_hg$Pos_coexpr_low) / nrow(df_tfrank_hg)
+# sum(df_tfrank_hg$Bind_high | df_tfrank_hg$Bind_low) / nrow(df_tfrank_hg)
+
+
+sum(df_tfrank_mm$Pos_coexpr_high) / nrow(df_tfrank_mm)
+sum(df_tfrank_mm$Bind_high) / nrow(df_tfrank_mm)
+
+
+df_mcgtf_hg <- mcgrank_check_if_motif_present(rcistarget_mcgtf_hg)
+df_mcgtf_mm <- mcgrank_check_if_motif_present(rcistarget_mcgtf_hg)
+
+
+
+
+
+sum(df_mcgtf_hg$Pos_high) / nrow(df_mcgtf_hg)
+sum(df_mcgtf_hg$Neg_high) / nrow(df_mcgtf_hg)
+
+
+sum(df_mcgtf_mm$Pos_high) / nrow(df_mcgtf_mm)
+sum(df_mcgtf_mm$Neg_high) / nrow(df_mcgtf_mm)
+
+
+# sum(df_mcgtf_hg$Pos_high | df_mcgtf_hg$Pos_low) / nrow(df_mcgtf_hg)
+# sum(df_mcgtf_hg$Neg_high | df_mcgtf_hg$Neg_low) / nrow(df_mcgtf_hg)
+
+view(df_mcgtf_hg[df_mcgtf_hg$Pos_high, ])
+
+
+
+
+
 
 
 # Inspecting top TF/motifs for a given gene
-# check_gene <- "DLL1"
-# 
-# df <- left_join(
-#   motif_rank@rankings[, c("motifs", check_gene)],
-#   motif_score@rankings[, c("motifs", check_gene)],
-#   by = "motifs",
-#   suffix = c("_rank", "_score")
-# ) %>%
-#   left_join(motifAnnotations, by = c("motifs" = "motif")) %>%
-#   arrange(!!sym(paste0(check_gene, "_rank")))
+check_gene <- "SPP1"
+
+df <- left_join(
+  motif_rank_hg@rankings[, c("motifs", check_gene)],
+  motif_score_hg@rankings[, c("motifs", check_gene)],
+  by = "motifs",
+  suffix = c("_rank", "_score")
+) %>%
+  left_join(motif_anno_hg, by = c("motifs" = "motif")) %>%
+  arrange(!!sym(paste0(check_gene, "_rank")))
 
