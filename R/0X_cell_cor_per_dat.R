e9ebaabe71e93ee790a61c0420d195898357b0c7
Alexander-Morin <alexm.14@outlook.com>
2025-02-04 21:02:48 -0800

    cleaning up/exporting plotting

--- R/0X_cell_cor_per_dat.R
+++ R/0X_cell_cor_per_dat.R
@@ -3,6 +3,7 @@
 
 library(tidyverse)
 library(aggtools)
+library(egg)
 source("R/00_config.R")
 source("R/utils/functions.R")
 
@@ -19,7 +20,7 @@ calc_cell_cor <- function(dat_l) {
   
   cor_l <- lapply(names(dat_l), function(x) {
     
-    print(message(paste(x, Sys.time())))
+    message(paste(x, Sys.time()))
     
     result <- tryCatch({
       cell_cor <- aggtools::sparse_pcor(dat_l[[x]]$Mat)
@@ -75,6 +76,8 @@ if (!file.exists(cell_cor_path)) {
 }
 
 
+stop()
+
 
 # TODO: some elements failed in lapply call, but can be regen isolated...
 # figure out how to do one pass
@@ -95,93 +98,90 @@ sample_cor <- calc_sample_cell_cor(dat_l[sample_cor_ids])
 input_l <- c(cor_l[!sapply(cor_l, is.null)], rerun_cor, sample_cor)
 
 
+# Setting up summary df for plotting
+
 plot_df <- do.call(rbind, input_l) %>% 
   as.data.frame() %>% 
   rownames_to_column(var = "ID") %>% 
   left_join(., mcg_meta, by = "ID") %>% 
   arrange(Median) %>%
-  # arrange(N_cells) %>% 
-  mutate(ID = factor(ID, levels = unique(ID)))
+  mutate(ID = factor(ID, levels = unique(ID)),
+         Platform = str_replace_all(Platform, " ", ""),
+         Platform = ifelse(is.na(Platform), "Mixed", Platform),
+         Platform = ifelse(sapply(str_split(Platform, ","), length) != 1, "Mixed", Platform))
 
 
-a <- ifelse(plot_df$Species == "Human", "royalblue", "goldenrod")
 
 
-ggplot(plot_df) +
+# cor.test(plot_df$Median, plot_df$N_cells, method = "spearman")
+# cor.test(plot_df$Median, plot_df$Median_UMI, method = "spearman")
+# table(plot_df$Platform)
+
+
+
+# Spread of intracellular correlations per dataset, colouring text by species
+
+# TODO: This throws a warning, need to figure out native support
+species_col <- ifelse(plot_df$Species == "Human", "royalblue", "goldenrod")
+
+p1 <- 
+  ggplot(plot_df) +
   geom_segment(aes(y = ID, x = `1st Qu.`, xend = `3rd Qu.`)) +
   geom_point(aes(y = ID, x = Median), shape = 21, colour = "black", fill = "firebrick", size = 3.4) +
   xlab("Cell to cell Pearson's correlation") +
   theme_classic() +
-  theme(axis.text.y = element_text(colour = a, size = 15),
+  theme(axis.text.y = element_text(colour = species_col, size = 15),
         axis.title.y = element_blank(),
-        axis.text.x = element_text(size = 15),
-        axis.title.x = element_text(size = 20))
+        axis.text.x = element_text(size = 20),
+        axis.title.x = element_text(size = 20),
+        plot.margin = margin(10, 30, 10, 10))
 
 
+ggsave(p1, dpi = 300, width = 9, height = 12, device = "png",
+       filename = file.path(plot_dir, "microglia_cell_to_cell_cor.png"))
 
-ggplot(plot_df, aes(x = log10(N_cells), y = Median)) +
+
+# Scatter of median intracellular cor with UMI and count of cells
+
+p2a <- 
+  ggplot(plot_df, aes(x = log10(N_cells), y = Median)) +
   geom_point(shape = 21, size = 2.4) +
   geom_smooth(method = lm, colour = "darkblue") +
   xlab("Log10 count of cells") +
   ylab("Median intra-cellular correlation") +
   theme_classic() +
-  theme(text = element_text(size = 25))
-
-
-cor.test(plot_df$Median, plot_df$N_cells, method = "spearman")
+  theme(text = element_text(size = 20))
 
 
 
-ggplot(plot_df, aes(x = log10(Median_UMI), y = Median)) +
+p2b <- 
+  ggplot(plot_df, aes(x = log10(Median_UMI), y = Median)) +
   geom_point(shape = 21, size = 2.4) +
   geom_smooth(method = lm, colour = "darkblue") +
   xlab("Log10 median UMI/UMI-like") +
   ylab("Median intra-cellular correlation") +
   theme_classic() +
-  theme(text = element_text(size = 25))
+  theme(text = element_text(size = 20))
+p2b_nolab <- p2b + theme(axis.title.y = element_blank())
 
 
-cor.test(plot_df$Median, plot_df$Median_UMI, method = "spearman")
+p2 <- ggarrange(p2a, p2b_nolab, nrow = 1)
+
+ggsave(p2, dpi = 300, width = 12, height = 5, device = "png",
+       filename = file.path(plot_dir, "microglia_median_intracell_cor_scatter.png"))
 
 
-tt <- plot_df
+# Boxplot of median intracellular cor with platform
 
-tt <- tt %>% 
-  mutate(Platform = str_replace_all(Platform, " ", ""),
-         Platform = ifelse(is.na(Platform), "Mixed", Platform),
-         Platform = ifelse(sapply(str_split(Platform, ","), length) != 1, "Mixed", Platform))
-
-
-table(tt$Platform)
-
-
-ggplot(tt, aes(x = reorder(Platform, Median), y = Median)) +
+p3 <- 
+  ggplot(plot_df, aes(x = reorder(Platform, Median), y = Median)) +
   geom_boxplot() +
-  geom_point(shape = 19) +
+  geom_jitter(shape = 21, fill = "slategrey", width = 0.1) +
   xlab(NULL) +
   ylab("Median intra-cellular correlation") +
   theme_classic() +
-  theme(text = element_text(size = 25),
+  theme(text = element_text(size = 15),
         axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
 
-
-
-
-# test_id <- "HPA"
-# test_cell_cor <- aggtools::sparse_pcor(dat_l[[test_id]]$Mat)
-# test_cell_cor <- mat_to_df(test_cell_cor, value_name = "Cor")
-# summary(test_cell_cor$Cor)
-# 
-# ggplot(test_cell_cor, aes(x = Cor)) +
-#   geom_density() +
-#   ggtitle(test_id) +
-#   xlab("Pairwise cell correlation") +
-#   ylab("Density") +
-#   theme_classic() +
-#   theme(text = element_text(size = 25))
-
-
-# keep_genes <- names(which(rowMeans(dat_l[[id]]$Mat) != 0))
-# cell_cor_keep <- aggtools::sparse_pcor(dat_l[[id]]$Mat[keep_genes, ])
-# cell_cor_keep <- mat_to_df(cell_cor_keep, value_name = "Cor")
-# summary(cell_cor_keep$Cor)
+ggsave(p3, dpi = 300, width = 12, height = 5, device = "png",
+       filename = file.path(plot_dir, "microglia_median_intracell_cor_platform_boxplot.png"))
